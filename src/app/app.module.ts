f0a5c96e8f2353e661fc88a9ff32c081938893c5
Tinus <jacksontinus96@gmail.com>
2017-12-05 13:22:16 +0200

    Added Login Basics with Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { CdkTableModule } from '@angular/cdk/table';
+import {
+  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatPaginatorModule,
+  MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule,
+  MatDialogModule, MatGridListModule, MatIconModule, MatInputModule,
+  MatListModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSortModule,
+  MatSlideToggleModule, MatSnackBarModule, MatTableModule, MatTabsModule, MatToolbarModule,
+  MatTooltipModule, MatFormFieldModule, MatExpansionModule, MatStepperModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
