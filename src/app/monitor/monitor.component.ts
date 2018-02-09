e2e6a18d6d015dd409c8f923c16d752443c236bf
Tinus <jacksontinus96@gmail.com>
2018-02-09 13:34:38 +0200

    update

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import 'tracking';
 import 'tracking/build/data/face';
-//import 'tracking/build/data/mouth';
-//import 'tracking/build/data/eye';
+import 'tracking/build/data/mouth';
+import 'tracking/build/data/eye';
 
 @Component({
     selector: 'monitor',
@@ -14,44 +14,43 @@ export class MonitorComponent implements OnInit {
 
     trackingTask: any;
     trackingList: Array<string> = ["face"];// "mouth", "eye"
+    monitor: boolean = false;
 
 
     constructor() { }
 
     ngOnInit() {
-        this.startTracking();
+        console.log();
     }
 
-    startTracking() {
+    startTrackingJS() {
         let video = document.getElementById('video');
         let canvas = <HTMLCanvasElement>document.getElementById('canvas');
-        let context = canvas.getContext('2d');
         let c = new tracking.ObjectTracker(this.trackingList);
 
         this.trackingTask = tracking.track(video, c, { camera: true });
-        this.trackingTask.run();
+
 
         c.on('track', function (event) {
-            context.clearRect(0, 0, canvas.width, canvas.height);
 
-            event.data.forEach(function (rect) {
-                console.log('PICKED UP [x: ' + rect.x + 'px ' + rect.x + rect.width + 5, rect.y + 11 + "]");
-                context.strokeStyle = '#a64ceb';
-                context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-                context.font = '11px Helvetica';
-                context.fillStyle = "#fff";
-                context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-                context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+            event.data.forEach(element => {
+                console.log(JSON.stringify(element));
             });
         });
     }
 
-    stopTracking() {
+    stopTrackingJS() {
         this.trackingTask.stop();
     }
 
 
+    switch() {
+        this.monitor = !this.monitor;
 
-
-
+        if (this.monitor) {
+            this.startTrackingJS();
+        } else {
+            //this.stopTrackingJS();
+        }
+    }
 }
--- src/app/services/ad-client.service.ts
+++ src/app/services/ad-client.service.ts
@@ -1,8 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/do";
+import 'rxjs/add/operator/timeout';
+import 'rxjs/add/operator/retryWhen';
 
 @Injectable()
 export class AdClientService {
 
-  constructor() { }
+  private _uniqueId: string;
+  private _timeout: number = 5000; // ms
+
+  setUniqueID(id: string): void {
+    this._uniqueId = id;
+  }
+
+  constructor(private _http: Http) { }
+
+  register(id: string): void {
+    // if succ then set unique id
+  }
+
+  ping(): boolean {
+    // Should be a validator 
+    if (this._uniqueId === undefined || this._uniqueId === null || this._uniqueId === '') {
+      return false;
+    }
+
+    return false;
+  }
+
+  uploadData(object: any) {
+    let headers = this.createRequestHeader();
+    let options = new RequestOptions({ headers: headers });
+    let entity: string = '';
+
+    return this._http.post("192.168.10.136:8080/api/att/upload/" + this._uniqueId, { object }, options)
+      .map(res => res.json());
+  }
+
+  private createRequestHeader() {
+    let headers = new Headers();
+
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    return headers;
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body;
+  }
 
 }
